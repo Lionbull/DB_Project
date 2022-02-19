7f88a5fed86cb3d0c6b9f3d9539e192839963c25
Lionbull <geo1529@yahoo.com>
2022-02-19 19:34:22 +0200

    Literally the whole project

---
+++ database.sql
@@ -0,0 +1,234 @@
+DROP TABLE IF EXISTS Customer;
+CREATE TABLE Customer (
+    ID_Customer INTEGER PRIMARY KEY AUTOINCREMENT,
+    Social_Number TEXT NOT NULL,
+    Name TEXT NOT NULL,
+    Address TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS Tablet;
+CREATE TABLE Tablet (
+    ID_Tablet INT NOT NULL PRIMARY KEY,
+    Manufacturer TEXT NOT NULL,
+    Model TEXT NOT NULL,
+    Memory INT NOT NULL,
+    Release_Year INT NOT NULL
+);
+
+DROP TABLE IF EXISTS Phone;
+CREATE TABLE Phone (
+    ID_Phone INT NOT NULL PRIMARY KEY,
+    Manufacturer TEXT NOT NULL,
+    Model TEXT NOT NULL,
+    Memory INT NOT NULL,
+    Release_Year INT NOT NULL
+);
+
+DROP TABLE IF EXISTS Product;
+CREATE TABLE Product (
+    ID_Product INT NOT NULL PRIMARY KEY,
+    Price INT NOT NULL
+);
+
+DROP TABLE IF EXISTS ShippingMethod;
+CREATE TABLE ShippingMethod (
+    ID_SM INT NOT NULL PRIMARY KEY,
+    Shipper TEXT NOT NULL,
+    Type TEXT
+);
+
+DROP TABLE IF EXISTS ShoppingCart;
+CREATE TABLE ShoppingCart (
+    ID_SC INT NOT NULL PRIMARY KEY,
+    ID_Customer INT NOT NULL,
+    Total_Price INT NOT NULL,
+    FOREIGN KEY (ID_Customer) REFERENCES Customer (ID_Customer)
+);
+
+DROP TABLE IF EXISTS Orders;
+CREATE TABLE Orders (
+    ID_Order INT NOT NULL PRIMARY KEY,
+    ID_SC INT NOT NULL,
+    Shipping INT NOT NULL,
+    Date TEXT NOT NULL,
+    FOREIGN KEY (ID_SC) REFERENCES ShoppingCart (ID_SC),
+    FOREIGN KEY (Shipping) REFERENCES ShippingMethod (ID_SM)
+);
+
+DROP TABLE IF EXISTS ProductsInShoppingCart;
+CREATE TABLE ProductsInShoppingCart (
+    ShoppingCart INT NOT NULL,
+    Product INT NOT NULL,
+    FOREIGN KEY (ShoppingCart) REFERENCES ShoppingCart (ID_SC),
+    FOREIGN KEY (Product) REFERENCES Product (ID_Product)
+);
+
+
+
+--Customer ID has no special pattern
+INSERT INTO Customer (Social_Number, Name, Address)
+VALUES
+    ("398-10-XXXX", "Alphius Oliver", "Keskiortentie 8"),
+    ("586-52-XXXX", "Shane Lug", "Mämminiementie 53"),
+    ("264-45-XXXX", "Lina Mina", "Korkeakoulunkatu 66"),
+    ("158-96-XXXX", "Cornélio Pietro", "Gesterbyntie 54"),
+    ("464-96-XXXX", "Prochorus Trudie", "Piilostentie 97"),
+    ("253-56-XXXX", "Dagmar Vesna", "Snellmaninkatu 83"),
+    ("124-31-XXXX", "Tsetseg Krishna", "Ysitie 53"),
+    ("038-53-XXXX", "Ameyalli Cléopâtre", "Otakaari 24"),
+    ("415-72-XXXX", "Bérenger Theia", "Siikarannantie 86"),
+    ("256-37-XXXX", "Secundinus Nebil", "Kiannonkatu 16"),
+    ("189-15-XXXX", "Sequoyah Nina", "Ysitie 58"),
+    ("463-26-XXXX", "Siva Bohuslav", "Kaarrostie 8"),
+    ("533-50-XXXX", "Ptolemy Malachy", "Oijärventie 48"),
+    ("562-61-XXXX", "Gorgi Lyubov", "Kartanomäenkatu 67"),
+    ("703-90-XXXX", "Lempi Blagoslav", "Rautatienkatu 76"),
+    ("394-84-XXXX", "Katelin Dragoș", "Kluuvikatu 72"),
+    ("765-99-XXXX", "Bertrand Eva", "Kanslerinrinne 60"),
+    ("311-33-XXXX", "Yiorgos Alethea", "Linnankatu 17"),
+    ("400-98-XXXX", "Arsène Waman", "Sulkuvartijankatu 82"),
+    ("232-48-XXXX", "Donata Guðlaug", "Tawastintie 41");
+
+
+
+--ID_Tablet has pattern "11XX"
+INSERT INTO Tablet (ID_Tablet, Manufacturer, Model, Memory, Release_Year)
+VALUES
+    (1101, "Apple", "iPad Air", "64", 2020),
+    (1102, "Apple", "iPad Air", "32", 2020),
+    (1103, "Apple", "iPad Pro", "128", 2021),
+    (1104, "Apple", "iPad Pro", "64", 2021),
+    (1105, "Apple", "iPad Mini", "64", 2021),
+    (1106, "Apple", "iPad Mini", "32", 2021),
+    (1107, "Samsung", "Galaxy Tab S5E", "128", 2020),
+    (1108, "Samsung", "Galaxy Tab S5E", "64", 2020),
+    (1109, "Microsoft", "Surface Pro 8", "128", 2019),
+    (1110, "Samsung", "Galaxy Tab S7 Plus", "64", 2019),
+    (1111, "Xiaomi", "Pad 5", "32", 2020),
+    (1112, "Samsung", "Galaxy Tab S6", "32", 2018);
+
+
+
+--ID_Phone has pattern "22XX"
+INSERT INTO Phone (ID_Phone, Manufacturer, Model, Memory, Release_Year)
+VALUES
+    (2201, "Apple", "iPhone 13 Pro", "128", 2021),
+    (2202, "Apple", "iPhone 13", "64", 2021),
+    (2203, "Apple", "iPhone 12 Pro", "128", 2019),
+    (2204, "Apple", "iPhone 12", "64", 2019),
+    (2205, "Apple", "iPhone 11 Pro", "128", 2018),
+    (2206, "Apple", "iPhone 11", "64", 2018),
+    (2207, "Asus", "ROG Phone 5s", "128", 2020),
+    (2208, "Xiaomi", "Mix 4", "32", 2020),
+    (2209, "Xiaomi", "11T Pro", "128", 2021),
+    (2210, "Google", "Pixel 6", "64", 2020),
+    (2211, "Sony", "Xperia 5 III", "128", 2019),
+    (2212, "Motorola", "Edge 20 Pro", "32", 2018),
+    (2213, "Xiaomi", "11 Lite 5G NE", "16", 2021),
+    (2214, "Honor", "50", "64", 2020),
+    (2215, "Huawei", "Nova 9", "32", 2019),
+    (2216, "realme", "GT Neo 2", "64", 2020);
+
+
+
+INSERT INTO Product (ID_Product, Price)
+VALUES
+    (1101, 600),
+    (1102, 500),
+    (1103, 800),
+    (1104, 650),
+    (1105, 400),
+    (1106, 350),
+    (1107, 250),
+    (1108, 225),
+    (1109, 900),
+    (1110, 200),
+    (1111, 150),
+    (1112, 100),
+    (2201, 1100),
+    (2202, 1000),
+    (2203, 900),
+    (2204, 850),
+    (2205, 850),
+    (2206, 800),
+    (2207, 1200),
+    (2208, 400),
+    (2209, 600),
+    (2210, 900),
+    (2211, 450),
+    (2212, 300),
+    (2213, 250),
+    (2214, 300),
+    (2215, 200),
+    (2216, 400);
+
+
+
+--ID_SM has pattern "33XX"
+INSERT INTO ShippingMethod (ID_SM, Shipper, Type)
+VALUES
+    (3301, "Posti", "Ground"),
+    (3302, "Matkahuolto", "Ground"),
+    (3303, "UPS", "Ship"),
+    (3304, "DHL", "Plane"),
+    (3305, "DB Schencker", "Ground");
+
+
+
+--ID_SC has pattern "44XX"
+INSERT INTO ShoppingCart (ID_SC, ID_Customer, Total_Price)
+VALUES
+    (4401, 1, 1100),
+    (4402, 2, 600),
+    (4403, 3, 850),
+    (4404, 4, 2200),
+    (4405, 5, 900),
+    (4406, 6, 600),
+    (4407, 7, 500),
+    (4408, 8, 1100),
+    (4409, 9, 1100),
+    (4410, 10, 1200),
+    (4411, 11, 1100),
+    (4412, 12, 900),
+    (4413, 13, 250),
+    (4414, 14, 350),
+    (4415, 15, 450);
+
+
+
+--ID_Order has pattern "55XX"
+INSERT INTO Orders (ID_Order, ID_SC, Shipping, Date)
+VALUES
+    (5501, 4401, 3301, "07/01/2019"),
+    (5502, 4403, 3301, "16/03/2019"),
+    (5503, 4402, 3301, "07/04/2019"),
+    (5504, 4407, 3302, "18/07/2019"),
+    (5505, 4408, 3301, "10/01/2020"),
+    (5506, 4405, 3301, "05/09/2020"),
+    (5507, 4411, 3304, "29/07/2021"),
+    (5508, 4409, 3303, "08/12/2021"),
+    (5509, 4410, 3301, "10/03/2022"),
+    (5510, 4412, 3305, "16/10/2022");
+
+INSERT INTO ProductsInShoppingCart (Shoppingcart, Product)
+VALUES
+    (4401, 2201),
+    (4402, 1101),
+    (4403, 1105),
+    (4403, 2211),
+    (4404, 2201),
+    (4404, 2201),
+    (4405, 1109),
+    (4406, 2209),
+    (4407, 1102),
+    (4408, 2201),
+    (4409, 2201),
+    (4410, 1101),
+    (4410, 1101),
+    (4411, 1102),
+    (4411, 1101),
+    (4412, 2210),
+    (4413, 2213),
+    (4414, 1106),
+    (4415, 1110),
+    (4415, 1107);
---
+++ project.py
@@ -0,0 +1,247 @@
+import sqlite3
+from bokeh.io import output_file, show
+from bokeh.plotting import figure
+
+def main():
+    print("(Use 1-20 for testing)")
+    current_user = input("Enter your user ID: ")
+    connection = sqlite3.connect('database.db')
+    while (True):
+        print("\nWhat do you want to do?")
+        print("1) View my orders")
+        print("2) View my shopping cart")
+        print("3) View my information")
+        print("4) Edit my information")
+        print("5) View products")
+        print("6) Create new shopping cart")
+        print("7) Create BOKEH bar chart")
+        print("0) Exit")
+        choise = int(input("Your choise: "))
+        if choise == 0:
+            print("Thank you!")
+            connection.commit()
+            connection.close()
+            break
+        elif choise == 1:
+            vieworders(connection, current_user)
+        elif choise == 2:
+            viewshoppingcart(connection, current_user)
+        elif choise == 3:
+            viewmyinfo(connection, current_user)
+        elif choise == 4:
+            editinformation(connection, current_user)
+        elif choise == 5:
+            viewproducts(connection)
+        elif choise == 6:
+            createnewsc(connection, current_user)
+        elif choise == 7:
+            createbokeh(connection)
+        else:
+            print("Unkown selection. Try again.\n")
+    
+    return 0
+
+
+def vieworders(connection, customer):
+    cursor = connection.cursor()
+    cursor.execute("""SELECT ShoppingCart.ID_SC FROM ShoppingCart
+                    INNER JOIN Orders
+                    ON ShoppingCart.ID_SC=Orders.ID_SC
+                    WHERE ShoppingCart.ID_Customer=?""",(customer,))
+    orders = cursor.fetchall()
+    if len(orders) == 0:
+        print("You haven't ordered anything yet!\n")
+    else:
+        print("Your order history (order numbers):")
+        for order in orders:
+            print(order)
+
+    
+    
+
+def viewshoppingcart(connection, customer):
+    """This function allow you view shopping cart"""
+    cursor = connection.cursor()
+    print("\nYour latest shopping cart:")
+    cursor.execute('SELECT MAX(ID_SC) FROM ShoppingCart WHERE ID_Customer=?',(customer,))
+    latest_sc = cursor.fetchall()[0][0]
+
+    cursor.execute("""SELECT Product.Manufacturer, Product.Model, Product.Memory 
+                    FROM ShoppingCart
+                    INNER JOIN ProductsInShoppingCart
+                    ON ShoppingCart.ID_SC=ProductsInShoppingCart.ShoppingCart
+                    INNER JOIN Product
+                    ON Product.ID_Product=ProductsInShoppingCart.Product
+                    WHERE ShoppingCart.ID_SC=?""",(latest_sc,))
+    rows = cursor.fetchall()
+    cursor.execute('SELECT Total_Price FROM ShoppingCart WHERE ID_SC=?',(latest_sc,))
+    total_price = cursor.fetchall()[0][0]
+    item = 1
+    for row in rows:
+        print("\nItem "+str(item)+":")
+        print("Name:",row[0],row[1])
+        print("Memory:",row[2])
+        item=item+1
+    print("\nTotal price:",total_price,)
+
+def viewmyinfo(connection, customer):
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM Customer WHERE ID_Customer=?',(customer,))
+    rows = cursor.fetchall()
+    print("\nYour information:\n")
+    for row in rows:
+        print("Customer ID:",row[0])
+        print("Name:",row[2])
+        print("SSN:",row[1])
+        print("Address:",row[3],)
+
+def editinformation(connection, customer):
+    cursor = connection.cursor()
+    while True:
+        print("\nWhat would you like to edit?")
+        print("1) Name")
+        print("2) SSN")
+        print("3) Address")
+        print("0) Exit")
+        editable = int(input("Choise: "))
+        if editable == 1:
+            name = input("New name: ")
+            cursor.execute('UPDATE Customer SET Name=? WHERE ID_Customer=?',(name, customer,))
+            connection.commit()
+            break
+        elif editable == 2:
+            ssn = input("New social security number: ")
+            cursor.execute('UPDATE Customer SET Social_Number=? WHERE ID_Customer=?',(ssn, customer,))
+            connection.commit()
+            break
+        elif editable == 3:
+            address = input("New address: ")
+            cursor.execute('UPDATE Customer SET Address=? WHERE ID_Customer=?',(address, customer,))
+            connection.commit()
+            break
+        elif editable == 0:
+            break
+        else:
+            print("Choose 1-3. Try again.")
+
+def viewproducts(connection):
+    cursor = connection.cursor()
+    while True:
+        print("\nWhat would you like to view?")
+        print("1) Phones")
+        print("2) Tablets")
+        print("3) The most expensive product")
+        print("4) The least expensive product")
+        print("0) Exit")
+        selection = int(input("Choise: "))
+        print("\n")
+        if selection == 1:
+            pattern_phone = "22%"
+            cursor.execute("SELECT Manufacturer, Model, Memory, Release_Year FROM Product WHERE ID_Product LIKE ?",(pattern_phone,))
+            rows = cursor.fetchall()
+            for row in rows:
+                print("{} {}, Memory: {}, Release year: {}".format(row[0],row[1],row[2],row[3]))
+            break
+        elif selection == 2:
+            pattern_tablet = "11%"
+            cursor.execute("SELECT Manufacturer, Model, Memory, Release_Year FROM Product WHERE ID_Product LIKE ?",(pattern_tablet,))
+            rows = cursor.fetchall()
+            for row in rows:
+                print("{} {}, Memory: {}, Release year: {}".format(row[0],row[1],row[2],row[3]))
+            break
+        elif selection == 3:
+            cursor.execute("SELECT Manufacturer, Model, Memory, Release_Year FROM Product WHERE MAX(Price)")
+            product = cursor.fetchall()
+            print("\nThe most expensive product is:")
+            print("Name: {} {}\nMemory: {}\nRelease year: {}\nPrice:".format(product[0][1],product[0][2],product[0][3],product[0][4],product[0][5]))
+        elif selection == 4:
+            pass
+        elif selection == 0:
+            break
+        else:
+            print("Unkown selection. Try again")
+
+def createnewsc(connection, customer):
+    cursor = connection.cursor()
+    cursor.execute("SELECT MAX(ID_SC) FROM ShoppingCart")
+    latest_sc=cursor.fetchall()[0][0]
+    latest_sc+=1
+    product_list=[]
+    while True:
+        print("\nWhat would you like to add?")
+        print("1) Phone")
+        print("2) Tablet")
+        print("3) Add selected")
+        print("0) Exit")
+        selection = int(input("Your choise: "))
+        if selection == 0:
+            break
+        elif selection == 1:
+            phone_pattern="22%"
+            cursor.execute("""SELECT * FROM Product WHERE ID_Product LIKE ?""",(phone_pattern,))
+            phone_list=cursor.fetchall()
+            print("Available products:")
+            for i in phone_list:
+                print(i)
+            id=int(input("Enter product id or 0 to exit: "))
+            if id == 0:
+                break
+            else:
+                product_list.append(id)
+        elif selection == 2:
+            tablet_pattern="11%"
+            cursor.execute("""SELECT *
+                            FROM Product
+                            WHERE ID_Product LIKE ?""",(tablet_pattern,))
+            phone_list=cursor.fetchall()
+            print("Available products:")
+            for i in phone_list:
+                print(i)
+            id=int(input("Enter product id or 0 to exit: "))
+            if id == 0:
+                break
+            else:
+                product_list.append(id)
+        elif selection == 3:
+            total_price=0
+            for id in product_list:
+                cursor.execute("""INSERT INTO ProductsInShoppingCart (Shoppingcart, Product)
+                                VALUES (?,?)""",(latest_sc, id,))
+                connection.commit()
+                cursor.execute("""SELECT Price FROM Product WHERE ID_Product=?""",(id,))
+                total_price = total_price + int(cursor.fetchone()[0])
+            cursor.execute("""INSERT INTO ShoppingCart (ID_SC, ID_Customer, Total_Price)
+                            VALUES (?,?,?)""",(latest_sc, customer, total_price,))
+            connection.commit()
+            print("Products added to shopping cart!")
+            
+
+
+def createbokeh(connection):
+    output_file("barchart.html")
+    cursor = connection.cursor()
+
+    cursor.execute("""SELECT Model FROM Product""")
+    names = []
+    for i in cursor.fetchall():
+        names.append(i[0])
+
+    cursor.execute("""SELECT Price FROM Product""")
+    prices = []
+    for i in cursor.fetchall():
+        prices.append(i[0])
+    
+    sorted_names = sorted(names, key=lambda x: prices[names.index(x)])
+
+    p = figure(x_range=sorted_names, height=500, width=2500, title="Product Prices",
+           toolbar_location=None, tools="")
+
+    p.vbar(x=names, top=prices, width=0.9)
+
+    p.xgrid.grid_line_color = None
+    p.y_range.start = 0
+
+    show(p)
+
+
+main()
